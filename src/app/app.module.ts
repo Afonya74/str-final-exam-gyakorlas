2648b354b1231528baf4b88d2c654932c0187d6c
Afonya74 <67206390+Afonya74@users.noreply.github.com>
2021-03-20 21:35:14 +0100

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './common/navbar/navbar.component';
+import { UserListComponent } from './page/user-list/user-list.component';
+import { UserEditorComponent } from './page/user-editor/user-editor.component';
+import { SearchPipe } from './pipe/search.pipe';
+import { SortPipe } from './pipe/sort.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    UserListComponent,
+    UserEditorComponent,
+    SearchPipe,
+    SortPipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/page/user-list/user-list.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { finalize } from 'rxjs/operators';
+import { User } from 'src/app/model/user';
+import { UserService } from 'src/app/service/user.service';
+
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.scss'],
+})
+export class UserListComponent implements OnInit {
+  users$: User[] = [];
+  phrase: string = '';
+  direction: number = 1;
+  columnKey: string = '';
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.refreshPage();
+  }
+
+  refreshPage(): void {
+    this.userService
+      .getAll()
+      .pipe(finalize(() => {}))
+      .subscribe(() => {});
+    this.userService.getAll().subscribe((items) => {
+      this.users$ = items;
+    });
+  }
+
+  onDelete(user: User): void {
+    alert('Are you sure you want to delete this user?');
+    this.userService.deleteUser(user).subscribe(() => {
+      this.userService.getAll();
+    });
+    this.refreshPage();
+  }
+
+  onChangePhrase(event: Event): void {
+    this.phrase = (event.target as HTMLInputElement).value;
+    this.refreshPage();
+  }
+
+  onColumnSelect(key: string): void {
+    if (this.columnKey === key) {
+      this.direction = this.direction * -1;
+    } else {
+      this.direction = 1;
+    }
+    this.columnKey = key;
+  }
+}
